dae879233201c6f1f94cf4c36f71fdec9f57c9b6
NHatim <hatim.naimi@gmail.com>
2021-01-09 14:40:02 +0100

    nav

---
+++ index.js
@@ -0,0 +1,6 @@
+const myImg = document.querySelector('.nav__github')
+myImg.addEventListener("click", myFunction);
+
+function myFunction() {
+  window.open("https://github.com/NHatim", '_blank');
+}
